edfae4dd84ed2b73663cd704c3b141a186077ff6
wldbs8563 <137027536+wldbs8563@users.noreply.github.com>
2023-08-03 18:10:47 +0900

    Add files via upload

---
+++ js/script/naver_point_pay.js
@@ -0,0 +1,6 @@
+const n_point_wrap = document.querySelector('.n_point_wrap')
+const user_point =  document.querySelector('#user_point')
+const use_point =  document.querySelector('#use_point')
+console.log(n_point_wrap, user_point, use_point)
+let a = 1000
+user_point = 
